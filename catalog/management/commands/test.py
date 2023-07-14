84d7df654f1d348d6be6af24497e60a7dc540d27
Андрей Марчук <marchuk-8282@mail.ru>
2023-07-14 22:27:35 +0200

    changed

--- catalog/management/commands/test.py
+++ catalog/management/commands/test.py
@@ -3,7 +3,6 @@ from django.core.management import BaseCommand
 from catalog.models import Product
 
 
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -12,13 +11,11 @@ class Command(BaseCommand):
         # pk_3 = Category.objects.get(pk=3)
         Product.objects.all().delete()
         product_list = [
-            {'name': 'Смартфон', 'description':'Asus', 'category':'телефоны', 'purchase_price':'15000'},
-            {'name': 'Планшет', 'description':'apple', 'category':'планшеты', 'purchase_price':'690000'},
-            {'name': 'Ноутбук', 'description':'Asus', 'category':'ноутбуки', 'purchase_price':'70000'}
+            {'name': 'Смартфон', 'description': 'Asus', 'category': 'телефоны', 'purchase_price': '15000'},
+            {'name': 'Планшет', 'description': 'apple', 'category': 'планшеты', 'purchase_price': '690000'},
+            {'name': 'Ноутбук', 'description': 'Asus', 'category': 'ноутбуки', 'purchase_price': '70000'}
         ]
         product_create = []
         for product_item in product_list:
             product_create.append(Product(**product_item))
         Product.objects.bulk_create(product_create)
-
-
