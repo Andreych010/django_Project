e7cee0f79dcd6d186bd8ec3587bc0e37a139ac9a
Андрей Марчук <marchuk-8282@mail.ru>
2023-07-21 22:51:47 +0200

    Зачистка

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,20 +8,4 @@ urlpatterns = [
     path('', home, name='home'),
     path("contacts/", contacts, name="contacts"),
     path("products/", products, name="products")
-    # path("contacts/", contacts, name="contacts")
-
 ]
-
-# from django.urls import path
-#
-# from catalog.apps import CatalogConfig
-# from catalog.views import contacts, home, products, product_card
-#
-# app_name = CatalogConfig.name
-#
-# urlpatterns = [
-#     path('contacts/', contacts, name='contacts'),
-#     path('', home, name='home'),
-#     path('products/', products, name='products'),
-#     path('product_card/<pk>/', product_card, name='product_card'),
-# ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-import os
-# from lib2to3.fixes.fix_input import context
 from catalog.models import Product, Category
 
-
-# from config import settings
 
 def home(request):
     category_list = Category.objects.all()
@@ -18,38 +14,15 @@ def home(request):
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        phone = request.POST.get('phone')
+        email = request.POST.get('email')
         message = request.POST.get('message')
-        print(f'Вам поступило новое сообщение: "{message}"\nname: {name}, phone: ({phone})')
+        print(f'Вам поступило новое сообщение: "{message}"\nname: {name}, phone: ({email})')
     context = {
         'title': 'Контакты'
     }
     return render(request, 'catalog/contacts.html', context)
 
-# def contacts(request):
-#     context = {'title': 'Контакты'}
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get("message")
-#         print(f'You have new message from {name}({phone}): {message}')
-#
-#     return render(request, 'catalog/contacts.html', context)
-#
-#
-# def home(request):
-#
-#     return render(request, 'catalog/home.html')
-# #
-#
-# def products_3(request):
-#     context = {
-#         'object_list': Product.objects.all()[:3],
-#         'title': 'Главная'
-#     }
-#     return render(request, 'catalog/products.html', context)
-#
-#
+
 def products(request):
     product_list = Product.objects.all()
     context = {
@@ -57,22 +30,3 @@ def products(request):
         'title': 'Товары'
     }
     return render(request, 'catalog/products.html', context)
-
-#
-#
-# def product_card(request, pk):
-#     product_item = Product.objects.get(pk=pk)
-#     context = {
-#         'object': product_item,
-#     # context = {
-#     #     'object_list': Product.objects.all(),
-#         'title': 'Карточка товара'
-#     }
-#     return render(request, 'catalog/product_card.html', context)
-#
-#
-# def index(request):
-#     path = settings.MEDIA_ROOT
-#     img_list = os.listdir(path + '/products')
-#     context = {'images': img_list}
-#     return render(request, "catalog/products.html", context)
